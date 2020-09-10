c5b99ee791ef7dd751af8fdfe5e0db0294a5e816
Felix Wong <fmhwong@ca.ibm.com>
2020-09-09 21:29:59 -0400

    Clean up

--- implementation-jaxrs2/src/main/java/io/smallrye/opentelemetry/jaxrs2/client/ClientSpanDecorator.java
+++ implementation-jaxrs2/src/main/java/io/smallrye/opentelemetry/jaxrs2/client/ClientSpanDecorator.java
@@ -37,17 +37,6 @@ public interface ClientSpanDecorator {
     ClientSpanDecorator STANDARD_TAGS = new ClientSpanDecorator() {
         @Override
         public void decorateRequest(ClientRequestContext requestContext, Span span) {
-            //            Tags.COMPONENT.set(span, "jaxrs");
-            //            Tags.PEER_HOSTNAME.set(span, requestContext.getUri().getHost());
-            //            Tags.PEER_PORT.set(span, requestContext.getUri().getPort());
-            //
-            //            Tags.HTTP_METHOD.set(span, requestContext.getMethod());
-            //
-            //            String url = URIUtils.url(requestContext.getUri());
-            //            if (url != null) {
-            //                Tags.HTTP_URL.set(span, url);
-            //            }
-
             SemanticAttributes.HTTP_METHOD.set(span, requestContext.getMethod());
             String url = URIUtils.url(requestContext.getUri());
             if (url != null) {
@@ -57,23 +46,7 @@ public interface ClientSpanDecorator {
 
         @Override
         public void decorateResponse(ClientResponseContext responseContext, Span span) {
-            //            Tags.HTTP_STATUS.set(span, responseContext.getStatus());
             SemanticAttributes.HTTP_STATUS_CODE.set(span, responseContext.getStatus());
         }
     };
-
-    /**
-     * As operation name provides HTTP path. If there are path parameters used in URL then
-     * spans for the same requests would have different operation names, therefore use carefully.
-     */
-    //    ClientSpanDecorator HTTP_PATH_OPERATION_NAME = new ClientSpanDecorator() {
-    //        @Override
-    //        public void decorateRequest(ClientRequestContext clientRequestContext, Span span) {
-    //            span.setOperationName(URIUtils.path(clientRequestContext.getUri()));
-    //        }
-    //
-    //        @Override
-    //        public void decorateResponse(ClientResponseContext response, Span span) {
-    //        }
-    //    };
 }
