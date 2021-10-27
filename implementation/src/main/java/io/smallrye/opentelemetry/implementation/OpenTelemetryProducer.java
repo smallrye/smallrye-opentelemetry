320c8a4cabf98e585f9779916ee328e2cbf4cd43
Roberto Cortez <radcortez@yahoo.com>
2021-10-27 15:10:27 +0100

    OpenTelemetry Prototype for CDI support

---
+++ implementation/src/main/java/io/smallrye/opentelemetry/implementation/OpenTelemetryProducer.java
@@ -0,0 +1,30 @@
+package io.smallrye.opentelemetry.implementation;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+
+@Singleton
+public class OpenTelemetryProducer {
+    @Inject
+    Instance<SpanExporter> exporters;
+
+    @Produces
+    @ApplicationScoped
+    public OpenTelemetry getOpenTelemetry() {
+        SdkTracerProviderBuilder tracerProviderBuilder = SdkTracerProvider.builder();
+        for (SpanExporter exporter : exporters) {
+            tracerProviderBuilder.addSpanProcessor(SimpleSpanProcessor.create(exporter));
+        }
+        return OpenTelemetrySdk.builder().setTracerProvider(tracerProviderBuilder.build()).buildAndRegisterGlobal();
+    }
+}
