25f3012697099b219f5513a0003b2ec5c79e35b5
Roberto Cortez <radcortez@yahoo.com>
2021-11-23 19:19:53 +0000

    Test that span name is not including query params

--- tck/src/test/java/io/smallrye/opentelemetry/tck/rest/RestSpanTest.java
+++ tck/src/test/java/io/smallrye/opentelemetry/tck/rest/RestSpanTest.java
@@ -5,6 +5,8 @@ import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SE
 import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_VERSION;
 import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.HTTP_METHOD;
 import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.HTTP_STATUS_CODE;
+import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.HTTP_TARGET;
+import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.HTTP_URL;
 import static io.restassured.RestAssured.given;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -82,6 +84,19 @@ class RestSpanTest {
         assertEquals(url.getPath() + "span/{name}", spanItems.get(0).getName());
         assertEquals(HTTP_OK, spanItems.get(0).getAttributes().get(HTTP_STATUS_CODE));
         assertEquals(HttpMethod.GET, spanItems.get(0).getAttributes().get(HTTP_METHOD));
+    }
+
+    @Test
+    void spanNameWithoutQueryString() {
+        given().get("/span/1?id=1").then().statusCode(HTTP_OK);
+
+        List<SpanData> spanItems = spanExporter.getFinishedSpanItems();
+        assertEquals(1, spanItems.size());
+        assertEquals(SERVER, spanItems.get(0).getKind());
+        assertEquals(url.getPath() + "span/{name}", spanItems.get(0).getName());
+        assertEquals(HTTP_OK, spanItems.get(0).getAttributes().get(HTTP_STATUS_CODE));
+        assertEquals(HttpMethod.GET, spanItems.get(0).getAttributes().get(HTTP_METHOD));
+        assertEquals(url.getPath() + "span/1?id=1", spanItems.get(0).getAttributes().get(HTTP_TARGET));
     }
 
     @Path("/")
