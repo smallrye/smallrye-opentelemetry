b3a8e1730e09108c61b9141c88957da2abb6a741
brunobat <brunobat@gmail.com>
2022-07-13 10:08:40 +0100

    Autoconfig Otel

--- implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/traces/TracesExporterConfig.java
+++ implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/traces/TracesExporterConfig.java
@@ -1,2 +1,24 @@
-package io.smallrye.opentelemetry.implementation.config;public interface TracesExporterConfig {
+package io.smallrye.opentelemetry.implementation.config.traces;
+
+import io.smallrye.config.WithDefault;
+import io.smallrye.opentelemetry.implementation.config.OtelConnectionConfig;
+
+import java.util.Optional;
+
+/**
+ * Path: otel.exporter.traces
+ */
+public interface TracesExporterConfig extends OtelConnectionConfig {
+
+    /**
+     * Enable tracing with OpenTelemetry.
+     * <p>
+     * This property is not available in the Open Telemetry SDK. It's Quarkus specific.
+     * <p>
+     * Support for tracing will be enabled if OpenTelemetry support is enabled
+     * and either this value is true, or this value is unset.
+     */
+    @WithDefault("true")
+    Optional<Boolean> enabled();
+
 }
