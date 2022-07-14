da89eb976043deb09776e37721221c88a2d78c4e
brunobat <brunobat@gmail.com>
2022-07-14 12:32:41 +0100

    property name fixes and dinamic classloaded values

--- implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/OtelExporterConfig.java
+++ implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/OtelExporterConfig.java
@@ -1,2 +1,9 @@
-package io.smallrye.opentelemetry.implementation.config;public interface OtelExporter {
+package io.smallrye.opentelemetry.implementation.config;
+
+import io.smallrye.opentelemetry.implementation.config.traces.TracesExporterConfig;
+
+public interface OtelExporterConfig extends OtelConnectionConfig{
+    TracesExporterConfig traces();
+    // TODO metrics();
+    // TODO logs();
 }
--- implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/traces/jaeger/ExporterJaegerTraceConfig.java
+++ implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/traces/jaeger/ExporterJaegerTraceConfig.java
@@ -1,8 +1,9 @@
-package io.smallrye.opentelemetry.implementation.config.traces.otlp;
+package io.smallrye.opentelemetry.implementation.config.traces.jaeger;
 
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.opentelemetry.implementation.config.OtelConnectionConfig;
 
-@ConfigMapping(prefix = "otel.exporter.otlp")
-public interface ExporterOtlpTraceConfig extends OtelConnectionConfig {
+@ConfigMapping(prefix = "otel.exporter.jaeger")
+public interface ExporterJaegerTraceConfig extends OtelConnectionConfig {
+    // TODO default Jaeger endpoint
 }
