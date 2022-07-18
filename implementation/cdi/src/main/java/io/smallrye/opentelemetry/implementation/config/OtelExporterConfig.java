1089ae93c62c3e5e24d2a7c3f1edf2958ce1e88f
brunobat <brunobat@gmail.com>
2022-07-18 12:20:45 +0100

    add namespace to default exporter

--- implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/OtelExporterConfig.java
+++ implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/config/OtelExporterConfig.java
@@ -1,7 +1,9 @@
 package io.smallrye.opentelemetry.implementation.config;
 
+import io.smallrye.config.ConfigMapping;
 import io.smallrye.opentelemetry.implementation.config.traces.TracesExporterConfig;
 
+@ConfigMapping(prefix = "otel.exporter.otlp")
 public interface OtelExporterConfig extends OtelConnectionConfig {
     TracesExporterConfig traces();
     // TODO metrics();
