7daac5e308d0241d75acbd556678bfb6ecbe3df3
Roberto Cortez <radcortez@yahoo.com>
2023-07-28 17:26:34 +0100

    Span and Baggage makeCurrent updates injected instances (#250)

--- implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/cdi/OpenTelemetryProducer.java
+++ implementation/cdi/src/main/java/io/smallrye/opentelemetry/implementation/cdi/OpenTelemetryProducer.java
@@ -4,7 +4,9 @@ import static io.smallrye.opentelemetry.api.OpenTelemetryConfig.INSTRUMENTATION_
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Disposes;
@@ -15,7 +17,13 @@ import jakarta.inject.Singleton;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.api.baggage.BaggageBuilder;
+import io.opentelemetry.api.baggage.BaggageEntry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
@@ -55,13 +63,93 @@ public class OpenTelemetryProducer {
     @Produces
     @RequestScoped
     public Span getSpan() {
-        return Span.current();
+        return new Span() {
+            @Override
+            public <T> Span setAttribute(final AttributeKey<T> key, final T value) {
+                return Span.current().setAttribute(key, value);
+            }
+
+            @Override
+            public Span addEvent(final String name, final Attributes attributes) {
+                return Span.current().addEvent(name, attributes);
+            }
+
+            @Override
+
+            public Span addEvent(
+                    final String name,
+                    final Attributes attributes,
+                    final long timestamp,
+                    final TimeUnit unit) {
+                return Span.current().addEvent(name, attributes, timestamp, unit);
+            }
+
+            @Override
+            public Span setStatus(final StatusCode statusCode, final String description) {
+                return Span.current().setStatus(statusCode, description);
+            }
+
+            @Override
+            public Span recordException(final Throwable exception, final Attributes additionalAttributes) {
+                return Span.current().recordException(exception, additionalAttributes);
+            }
+
+            @Override
+            public Span updateName(final String name) {
+                return Span.current().updateName(name);
+            }
+
+            @Override
+            public void end() {
+                Span.current().end();
+            }
+
+            @Override
+            public void end(final long timestamp, final TimeUnit unit) {
+                Span.current().end(timestamp, unit);
+            }
+
+            @Override
+            public SpanContext getSpanContext() {
+                return Span.current().getSpanContext();
+            }
+
+            @Override
+            public boolean isRecording() {
+                return Span.current().isRecording();
+            }
+        };
     }
 
     @Produces
     @RequestScoped
     public Baggage getBaggage() {
-        return Baggage.current();
+        return new Baggage() {
+            @Override
+            public int size() {
+                return Baggage.current().size();
+            }
+
+            @Override
+            public void forEach(final BiConsumer<? super String, ? super BaggageEntry> consumer) {
+                Baggage.current().forEach(consumer);
+            }
+
+            @Override
+            public Map<String, BaggageEntry> asMap() {
+                return Baggage.current().asMap();
+            }
+
+            @Override
+            public String getEntryValue(final String entryKey) {
+                return Baggage.current().getEntryValue(entryKey);
+            }
+
+            @Override
+            public BaggageBuilder toBuilder() {
+                return Baggage.current().toBuilder();
+            }
+        };
     }
 
     void close(@Disposes final OpenTelemetry openTelemetry) {
