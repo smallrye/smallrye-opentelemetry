60508e154b25964876dd08c58226f9299170c8e3
Jason Lee <jason@steeplesoft.com>
2023-10-26 17:16:35 -0500

    [vertx_quarkus_fork]
    Add fork of Quarkus vertx-based exporters
    Create new module for exporters
    Add testcontainer-based testing of new exporters

---
+++ implementation/exporters/src/main/java/io/smallrye/opentelemetry/implementation/exporters/BufferOutputStream.java
@@ -0,0 +1,25 @@
+package io.smallrye.opentelemetry.implementation.exporters;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import io.vertx.core.buffer.Buffer;
+
+final class BufferOutputStream extends OutputStream {
+
+    private final Buffer buffer;
+
+    public BufferOutputStream(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        buffer.appendBytes(b, off, len);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        buffer.appendInt(b);
+    }
+}
